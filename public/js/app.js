ee83ddd9da92d4dd6306e4a118c8a163ba9c058a
James Mwangi <karokijames40@gmail.com>
2016-04-04 21:19:25 +0300

    Set up happenings client routing, controllers and views

--- public/js/app.js
+++ public/js/app.js
@@ -40,6 +40,10 @@ mkatolikiApp.config(['$routeProvider', function($routeProvider){
            templateUrl: 'partials/reflections/index.html',
             controller: 'ReflectionController'
     }).
+    when('/happenings', {
+            templateUrl: 'partials/happenings/index.html',
+            controller: 'HappeningController'
+    }).
     otherwise({
             redirectTo: '/'
     });
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -414,11 +414,132 @@ mkatolikiAppControllers.controller('ReflectionController', ['$scope', '$http', '
 
 }]);
 
+mkatolikiAppControllers.controller('HappeningController', ['$scope', '$http', 'userService', 'happeningService', function($scope, $http , userService, happeningService){
+
+    $scope.refresh = function(){
+
+        $scope.$emit('LOAD');
+
+
+        happeningService.getAll(function(response){
+
+            $scope.happenings = response;
+
+            $scope.$emit('UNLOAD');
+
+        }, function(){
+
+            alert('Some errors occurred while communicating with the service, Try Again Later');
+        });
+    }
+
+    $scope.create = function(){
+
+        happeningService.create({
+
+            event_title: $scope.currentTitle,
+            event_body: $scope.currentBody,
+            event_excerpt: $scope.currentExcerpt,
+            event_date: $scope.currentDate
+
+
+        }, function(){
+
+            $('#addHappeningModal').modal('toggle');
+
+            $scope.currentHappeningReset();
+            $scope.refresh();
+        }, function(response){
+
+            alert('Some error occurred while creating the happening event');
+
+        });
+    }
+
+    $scope.remove = function(happeningId){
+
+        if(confirm('Are you sure you want to remove this happening event?')){
+
+            happeningService.remove(happeningId, function(){
+
+                alert('Happening event removed Successfully.');
+                $scope.refresh();
+
+            }, function(){
+                alert('Some errors occurred while deleting the happening event')
+            });
+        }
+    }
+
+    $scope.currentHappeningReset = function(){
+
+        $scope.currentTitle = "";
+        $scope.currentBody = "";
+        $scope.currentExcerpt = "";
+        $scope.currentDate = "";
+
+    }
+
+
+    $scope.load = function(happeningId){
+
+        happeningService.getById(happeningId, function(response){
+
+            $scope.currentHappeningId = response.happening.id;
+            $scope.currentTitle = response.reflection.event_title;
+            $scope.currentBody = response.reflection.event_body;
+            $scope.currentDate = response.reflection.event_date;
+
+
+
+            $('#updateHappeningModal').modal('toggle');
+
+
+
+        }, function(){
+
+            alert('Some Errors Occurred, Try again later');
+        });
+    }
+
+    $scope.update = function(){
+
+        happeningService.update(
+
+            $scope.currentHappeningId,
+            {
+                event_title: $scope.currentTitle,
+                event_body: $scope.currentBody,
+                event_excerpt: $scope.currentExcerpt,
+                event_date: $scope.currentDate
+            },function(response){
+
+                $('#updateHappeningModal').modal('toggle');
+                $scope.currentHappeningReset();
+                $scope.refresh();
+
+            }, function(response){
+
+                alert('Some error occurred while updating, Try again');
+            }
+        );
+    }
+
+    if(!userService.checkIfLoggedIn()){
+
+        $location.path('/login');
+    }
+
+    $scope.happenings = [];
+
+    $scope.refresh();
+    $scope.currentHappeningReset();
+
+}]);
+
+
 
 mkatolikiAppControllers.controller('MainController', ['$scope', '$http', '$location', 'userService', 'mainService', function($scope, $http, $location, userService, mainService){
-
-
-
 
     var in10Days = new Date();
     in10Days.setDate(in10Days.getDate() + 10);
