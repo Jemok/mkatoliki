38c0c7b75a041ab6d7351ff8098abd37bb89204f
James Mwangi <karokijames40@gmail.com>
2016-04-29 00:41:00 +0300

    Sets add/update for gcm/tokens endpoint

--- app/Api/V1/Controllers/PhoneTokenController.php
+++ app/Api/V1/Controllers/PhoneTokenController.php
@@ -22,12 +22,24 @@ class PhoneTokenController extends Controller
 
         $phone_token = new Phone_token;
 
+        if($this->currentUser()->phone_token()->exists()){
+
+           if($this->currentUser()->phone_token()->update([
+                'token' => $request->get('token')
+            ])){
+
+               return $this->response->created();
+           }else{
+               return $this->response->error('could_not_create_token', 500);
+           }
+        }
+
         $phone_token->token = $request->get('token');
 
         if($this->currentUser()->phone_token()->save($phone_token))
             return $this->response->created();
         else
-            return $this->response->error('could_not_create_prayer', 500);
+            return $this->response->error('could_not_create_token', 500);
 
     }
 
