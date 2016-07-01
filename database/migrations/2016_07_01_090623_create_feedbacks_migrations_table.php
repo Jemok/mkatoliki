156c4ad3d7bba417e0de7cd947723c9bebe59ee4
James Mwangi <karokijames40@gmail.com>
2016-07-01 09:11:44 +0300

    Adds feedback migration

---
+++ database/migrations/2016_07_01_090623_create_feedbacks_migrations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeedbacksMigrationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('mood')->unsigned();
+            $table->text('comment');
+            $table->integer('user_id')->index()->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('feedbacks');
+    }
+}
