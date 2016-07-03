b1a94e83a8b443e9a92aa7a153ab1d6c16da38dc
James Mwangi <karokijames40@gmail.com>
2016-07-03 11:15:55 +0300

    Changes text size of confirm email success message

--- resources/views/verification/success.blade.php
+++ resources/views/verification/success.blade.php
@@ -1,12 +1,12 @@
 <link href="{{asset('css/bootstrap.min.css')}}" type="text/css" rel="stylesheet">
 
-<div class="container" style="padding-top:72px;">
+<div class="container" style="padding-top:200px;">
      <div class="row">
               @if(Session::has("flash_message"))
                     <div class="row">
                         <div class="col-md-11 col-md-offset-1">
                             <div class="alert alert-success alert-dismissible" role="alert">
-                                <strong>{{session("flash_message")}}</strong>
+                                <h1>{{session("flash_message")}}</h1>
                             </div>
                         </div>
                     </div>
